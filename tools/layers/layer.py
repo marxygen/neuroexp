6f134ffd681a4d8f32bff1499e1e94f1b3f382d0
Mark Ivchenko <marxygen@gmail.com>
2022-02-08 18:09:55 +0300

    Work on neural network

--- tools/layers/layer.py
+++ tools/layers/layer.py
@@ -1,11 +1,34 @@
+import numpy as np
+from typing import Union
+from importlib import import_module
+from abc import abstractmethod
+
 
 class Layer(object):
-    def __init__(self, neurons: int, inputs: int):
+    weights: np.array
+    biases: np.array
+    product: np.array
+    activated = np.array
+
+    def __init__(self, neurons: int, inputs: int, activation: Union[str, callable], **kwargs):
         """Create a new Layer
 
         Args:
             neurons (int): Number of neurons
             inputs (int): Number of inputs each neuron has
         """
-        # Weights are generated in the required shape
-        self.weights = np.random.randn((inputs, neurons))
+        self.weights = np.random.randn(inputs, neurons)
+        self.biases = np.zeros(neurons)
+        self.activation = (getattr(import_module(f'activations.{activation}'), activation)
+                           if not callable(activation) else activation
+                           )
+        self.activation_deriv = (getattr(import_module(f'activations.{activation}'), activation + '_deriv')
+                           if not callable(activation) else kwargs['activation_deriv']
+                           )
+        @abstractmethod
+        def forward(self, *args, **kwargs):
+            ...
+
+        @abstractmethod
+        def backwards(self, *args, **kwargs):
+            ...
--- tools/neural_network.py
+++ tools/neural_network.py
@@ -1,9 +1,43 @@
+import numpy as np
+from typing import Union
+from importlib import import_module
 
 
 class NeuralNetwork(object):
-    def __init__(self, layers: list):
+    def __init__(self,
+                 loss: Union[str, callable],
+                 layers: list,
+                 learning_rate=0.01,
+                 **kwargs
+                 ):
         """Create a neural network
 
         Args:
             layers (list): List of neural network layers
         """
+        self.layers = layers
+        self.loss = (getattr(import_module(f'losses.{loss}'), loss)
+                     if not callable(loss) else loss
+                     )
+        self.loss_deriv = (getattr(import_module(f'losses.{loss}'), loss + '_deriv')
+                           if not callable(loss) else kwargs['loss_deriv']
+                           )
+        self.learning_rate = learning_rate
+
+    def perform_forward(self, inputs: np.array, desired_outputs: np.array):
+        """Perform forward pass"""
+        data = inputs
+
+        for layer in self.layers:
+            data = layer.forward(data)
+
+        loss = self.loss(X=data, Y=desired_outputs)
+        print('Loss:', loss)
+        return data
+
+    def perform_backward(self, inputs, desired_outputs):
+        """Perform backward pass"""
+        # Initialize with the derivative of loss function
+        dvalue = self.loss_deriv(X=inputs, Y=desired_outputs)
+
+        self.layers[0].backwards(dvalue=dvalue, learning_rate=self.learning_rate, next=self.layers[1:])
